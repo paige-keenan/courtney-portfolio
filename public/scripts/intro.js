82851a2c9fbc8709fe82822d07496f3610d83eb1
Paige Keenan <paikeena@usatlrad3007.global.publicisgroupe.net>
2015-09-02 10:43:44 -0400

    create intro js file

---
+++ public/scripts/intro.js
@@ -0,0 +1,37 @@
+(function() {
+  
+  var names = ['Courtney Perets', 'Coatney', 'Coats', 'C-Peasy', 'Shortney'];
+       
+  function init() {
+    rearrangeName();
+  }
+  
+  function rearrangeName() {
+    var myIndex = 1;
+    var print = $('.container h1');
+
+    print.text(names[0]);
+
+    print.on('click', function() {
+      print.addClass('hideName');
+
+      setTimeout( function(){ 
+          nextElement();
+      }  , 400 );      
+      
+      setTimeout( function(){ 
+          print.removeClass('hideName');
+      }  , 400 );              
+    });
+
+    function nextElement() {
+      print.text(names[myIndex]);
+      myIndex = (myIndex + 1) % (names.length);
+    }
+  }
+  
+  $(document).on('ready', function() {
+      init();
+  });
+  
+})();
