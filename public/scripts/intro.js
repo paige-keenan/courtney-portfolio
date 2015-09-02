9e7e275f964b7df0bfddd8bc28bb1e5968e77f86
Paige Keenan <paikeena@usatlrad3007.global.publicisgroupe.net>
2015-09-02 15:54:58 -0400

    add image cycle through

--- public/scripts/intro.js
+++ public/scripts/intro.js
@@ -1,9 +1,12 @@
 (function() {
   
   var names = ['Courtney Perets', 'Coatney', 'Coats', 'C-Peasy', 'Shortney'];
+  var images = ['../../images/intro/profPic01.jpg', ' ../../images/intro/profPic02.jpg', '../../images/intro/profPic03.jpg', '../../images/intro/profPic04.jpg'];
        
   function init() {
     rearrangeName();
+    applyImageOverlay();
+
   }
   
   function rearrangeName() {
@@ -28,6 +31,22 @@
       print.text(names[myIndex]);
       myIndex = (myIndex + 1) % (names.length);
     }
+  }
+
+  function applyImageOverlay() {
+    var myIndex = 1;
+    var imageSwap = $('.intro__container__image-portrait');
+
+    imageSwap.attr('src', images[0]);
+
+    imageSwap.on('click', function() {
+      nextImage();
+    });
+
+    function nextImage() {
+      imageSwap.attr('src', images[myIndex]);
+      myIndex = (myIndex + 1) % (images.length);
+    }   
   }
   
   $(document).on('ready', function() {
